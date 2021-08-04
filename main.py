59639939a575af3ebeeda97a35c3f1e857aa5823
djvoelker <djvoelker28@outlook.com>
2021-08-03 21:06:53 -0400

    renamed files

--- main.py
+++ main.py
@@ -1,62 +1,96 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Aug  1 19:06:52 2021
+import tkinter as tk
+import main
 
-@author: djvoe
+window = tk.Tk()
+window.title("Binary Number Search")
+window.geometry("400x400")
+
+times_played = 0
+lists_generated = 0
+lists_shown = 0
+list_visible = "Show"
+
+label1 = tk.Label()
+label1.grid(column=0, row=0)
+
+label3 = tk.Label()
+label3.grid(column=0, row=6)
 
 
-Example setup:
+def gen_list():
+    global generated_list
+    global label1
+    global lists_generated
+    generated_list = main.list_gen(20, 100, 1, 100)
+    label1.config(text=f"This program just generated a list of {len(generated_list)} random integers from {generated_list[0]} to {generated_list[-1]}.")
+    lists_generated += 1
 
-1. generate random list
+gen_list()
 
-    generated_list = list_gen(20, 100, 1, 100)
+label3.config(text=f"You have searched for {times_played} numbers and generated {lists_generated} lists.")
 
-2. ask user what number they would like to find
+def makelistdisplay():
+    global display2
+    display2 = tk.Text(master=window, width=30, height=10, exportselection=False, wrap="word")
+    display2.grid(column=0, row=8)
+    display2.insert(tk.END, generated_list)
 
-    user_response = int(input("What number would you like to find? "))
+def execute(): 
+    global user_response
+    global num_index
+    global generated_list
+    user_response = int(entry1.get())
+    entry1.delete(0, len(entry1.get())+1)
+    num_index = main.search(generated_list, user_response)
+
+def display_index():
+    global times_played
+    execute()
+    answer = main.respond(num_index, user_response)
+    display = tk.Text(master=window, width=30, height=2, exportselection=False, wrap="word")
+    display.grid(column=0, row=4)
+    display.insert(tk.END, answer)
+    times_played += 1
     
-3. search list for number
+    label3.config(text=f"You have searched for {times_played} numbers and generated {lists_generated} lists.")
+
+def show_list():
+    global lists_shown
+    global generated_list
+    global list_visible
+    global button3
+    if list_visible == "Show":
+        list_visible = "Hide"
+        makelistdisplay()
+    else:
+        list_visible = "Show"
+        display2.destroy()
+    button3.config(text=f"{list_visible} list")
+    lists_shown += 1
     
-    index_of_user_response = search(generated_list, user_response)
-    
-4. respond to user accordingly
-    
-    print(respond(index_of_user_response, user_response))
-"""
+def button2_pressed():
+    global display2
+    gen_list()
+    label3.config(text=f"You have searched for {times_played} numbers and generated {lists_generated} lists.")
+    if list_visible == "Hide":
+        print(generated_list[0])
+        #for some reason you need to use "1.0" instead of 0 and "end" instead of len(generated_list)-1 for the indices.
+        display2.delete("1.0", "end")
+        display2.insert(tk.END, generated_list)
 
-#must import both of these
-from random import randint
-from math import floor
+label2 = tk.Label(text="What number would you like to search for in the list?")
+label2.grid(column=0, row=1)
 
-#generates a new list of unique integers of specified length and number range
-def list_gen(min_size, max_size, min_num, max_num):
-    i = []
-    for x in range(randint(min_size, max_size)):
-        i.append(randint(min_num, max_num))
-    i = list(set(i))
-    i.sort()
-    return i
+entry1 = tk.Entry(exportselection=False)
+entry1.grid(column=0, row=2)
 
-#uses binary search to search for x in list l and returns index of number if found in list
-def search(l, x):
-    bounds = [0, len(l)-1]  
-    done = False
-    while done == False:
-        if bounds[1]-bounds[0] > 1:
-            if l[floor(bounds[0]+(bounds[1]-bounds[0])/2)] > x: bounds[1] = floor(bounds[0]+(bounds[1]-bounds[0])/2)
-            elif l[floor(bounds[0]+(bounds[1]-bounds[0])/2)] < x: bounds[0] = floor(bounds[0]+(bounds[1]-bounds[0])/2)
-            else:
-                index = floor((bounds[0]+(bounds[1]-bounds[0])/2))
-                done = True
-        else:
-            if l[bounds[1]] == x: index = bounds[1]
-            elif l[bounds[0]] == x: index = bounds[0]
-            else: index = "none"
-            done = True
-    return index
+button1 = tk.Button(text="Search for number", command=display_index)
+button1.grid(column=0, row=3)
 
-#returns whether given number was found and the index of the number in the list given
-def respond(pos, num):
-    if pos == "none": i = str("The number " + str(num) + " is not in the list.")
-    else: i = str("The number " + str(num) + " is at position " + str(pos+1)+ " in the list.")
-    return i
+button2 = tk.Button(text="Generate new list", command=button2_pressed)
+button2.grid(column=0, row=5)
+
+button3 = tk.Button(text=f"{list_visible} list", command=show_list)
+button3.grid(column=0, row=7)
+
+window.mainloop()
